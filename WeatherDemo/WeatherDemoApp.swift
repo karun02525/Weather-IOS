e915e9e2d95accbbe1f3e3adad59d8142eab45c8
karunkumar <karun.kumar@hidglobal.com>
2024-04-18 14:46:55 +0530

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Karun Kumar on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
